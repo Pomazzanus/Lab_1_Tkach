7569c934179ae2b9724ff77f2a8453d2a3b2843a
Pomazzanus <97071607+Pomazzanus@users.noreply.github.com>
2022-10-18 23:40:34 +0300

    Update Repository.java

--- src/Repository.java
+++ src/Repository.java
@@ -1,16 +1,15 @@
 public class Repository {
 
-    public void Repository(){
+    public OperationMatrix Get_repository(){
         // варіант 'Ю'
-        String Matr_27[][] = { {"Т1", "С1", "Т2","Т4","Т5"," "," "},
+        String[][] Matr_27 = { {"Т1", "С1", "Т2","Т4","Т5"," "," "},
                              {"Т5", "Т1", "Т3","Т2","Т4"," "," "},
                              {"Т5", "С1", "Т1","Т2","Т3","Ф2","Ф3"},
                              {"Т1", "С1", "Т2","Т3","Т4"," "," "},
                              {"Т2", "С2", "Т3","Т4","Т5"," "," "},
                              {"Т1", "С3", "Ф2","Т4","Т5"," "," "},
                              {"Т1", "С1", "Т2","Т3","С2","Т4","Т5"},};
-        OperationMatrix OperationMatrix_27 = new OperationMatrix(Matr_27);
-        OperationMatrix_27.display();
+        return new OperationMatrix(Matr_27);
     }
 
 }
